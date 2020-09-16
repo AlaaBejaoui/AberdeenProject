b44cf79e27d9c2874fe49bdd8c6871a40bbf8101
Maha <maha_badri@yahoo.fr>
2020-09-17 00:24:01 +0200

    fix autodoc

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -2,6 +2,7 @@
 #
 import os
 import sys
+sys.path.insert(0, os.path.abspath('../'))
 sys.path.insert(0, os.path.abspath('../../AberdeenProject/'))
 
 # -- Project information -----------------------------------------------------
@@ -29,7 +30,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.githubpages',
     'sphinx.ext.viewcode',
-    'autoapi.extension'
+    'sphinx.ext.autodoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
